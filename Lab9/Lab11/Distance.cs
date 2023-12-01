caf1ee2732290bf3412b1942f004032900a9ff5e
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-12-01 16:05:56 +0300

    Add files via upload

---
+++ Lab9/Lab11/Distance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Distance
+    {
+        public int feet { get; set; }
+        public double inches { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", feet, inches);
+        }
+        public static bool operator !=(Distance A1, Distance A2)
+        {
+            if (A1.feet == A2.feet)
+                return true;
+            return false;
+        }
+        public static bool operator ==(Distance A1, Distance A2)
+        {
+            if (A1.feet != A2.feet)
+                return true;
+            return false;
+        }
+        public override bool Equals(object o)
+        {  
+       return true;  
+         }
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+}
+        
